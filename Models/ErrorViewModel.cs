4a7243bf2db1636bbabf7fef97c813ce1492c56a
Han_Yoo <poxsstar@gmail.com>
2025-10-27 01:34:01 +1300

    Initial commit of Onewhero Bay Visitor Management System

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Visitor_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
